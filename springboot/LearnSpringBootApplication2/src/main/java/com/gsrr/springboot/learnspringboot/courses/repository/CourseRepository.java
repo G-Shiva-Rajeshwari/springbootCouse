0635e3d07e1c99b2a200d5af541e8a2f80ffdd78
G-Shiva-Rajeshwari <shivarajeshwari333@gmail.com>
2026-01-08 10:11:21 +0530

    Delete springboot directory

--- springboot/LearnSpringBootApplication2/src/main/java/com/gsrr/springboot/learnspringboot/courses/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gsrr.springboot.learnspringboot.courses.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gsrr.springboot.learnspringboot.courses.bean.controller.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-
-}
--- springboot/LearnSpringBootApplication2/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into COURSE (ID, AUTHOR, COURSE)
-values (100001, 'in28minutes', 'Learn Microservices'); 
-insert into COURSE (ID, AUTHOR, COURSE)
-values (100002, 'in28minutes', 'Learn FullStack with React and Angular'); 
-insert into COURSE (ID, AUTHOR, COURSE)
-values (100003, 'in28minutes', 'Learn AWS, GCP and Azure'); 
